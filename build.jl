f2ba2c5d4c1e186dc945fdee48ebfb741814239b
Tim Siebert <siebert@tu-berlin.de>
2023-11-10 12:07:03 +0100

    improve the cmake file and build.jl

--- build.jl
+++ build.jl
@@ -1,3 +1,10 @@
+using Pkg
+
+
+Pkg.add(url="git@github.com:TimSiebert1/ADOLC_jll.jl.git")
+Pkg.add("libcxxwrap_julia_jll")
+Pkg.add("CxxWrap")
+
 using ADOLC_jll
 using libcxxwrap_julia_jll
 
@@ -7,5 +14,5 @@ ADOLC_DIR = ADOLC_jll.artifact_dir
 JlCxx_DIR = joinpath(libcxxwrap_julia_jll.artifact_dir, "lib", "cmake", "JlCxx")
 
 # Build!
-run(`cmake -DADOLC_DIR=$(ADOLC_DIR) -DJlCxx_DIR=$(JlCxx_DIR) -B$(build_DIR) -S$(src_DIR)`)
+run(`cmake -DADOLC_DIR=$(ADOLC_DIR) -DJlCxx_DIR=$(JlCxx_DIR) -S$(src_DIR) -B$(build_DIR) `)
 run(`cmake --build $(build_DIR)`)
