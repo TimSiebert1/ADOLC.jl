0b4fa2e8e73ebcc335bd04182ead383835fa2abd
Tim Siebert <tim.siebert96@outlook.com>
2023-11-13 10:22:55 +0100

    now one can use Pkg.add

--- src/ADOLC_wrap.jl
+++ src/ADOLC_wrap.jl
@@ -1,5 +1,10 @@
 module ADOLC_wrap
+  using CxxWrap
+  @wrapmodule(() -> joinpath("../build/","libtest_power"))
 
-greet() = print("Hello World!")
-
+  function __init__()
+    @initcxx
+  end
+  include("ops.jl")
+export trace_on, trace_off, adouble, getValue, forward, myalloc2, alloc_vec, getindex_mat, setindex_mat, getindex_vec, setindex_vec, reverse2
 end # module ADOLC_wrap
