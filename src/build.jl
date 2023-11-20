ed5d3aa0a07b443830d3217771177add3c341771
Tim Siebert <tim.siebert96@outlook.com>
2023-11-20 10:20:01 +0100

    add automatic detection of Julia_PREFIX

--- src/build.jl
+++ src/build.jl
@@ -14,12 +14,10 @@ src_DIR = "."
 ADOLC_DIR = ADOLC_jll.artifact_dir
 JlCxx_DIR = joinpath(libcxxwrap_julia_jll.artifact_dir, "lib", "cmake", "JlCxx")
 
-#Julia_PREFIX=/PATH/TO/JULIA
-#Julia_EXECUTABLE=/PATH/TO/BIN/JULIA
+Julia_PREFIX = split(Sys.BINDIR, "/bin")[1]
 
 # Build!
-#run(`cmake -DADOLC_DIR=$(ADOLC_DIR) -DJlCxx_DIR=$(JlCxx_DIR) Julia_PREFIX=$(Julia_PREFIX) -DJulia_EXECUTABLE=$(Julia_EXECUTABLE) -S$(src_DIR) -B$(build_DIR)`)
-run(`cmake -DADOLC_DIR=$(ADOLC_DIR) -DJlCxx_DIR=$(JlCxx_DIR) -S$(src_DIR) -B$(build_DIR)`)
+run(`cmake -DADOLC_DIR=$(ADOLC_DIR) -DJlCxx_DIR=$(JlCxx_DIR) -DJulia_PREFIX=$(Julia_PREFIX) -S$(src_DIR) -B$(build_DIR)`)
 run(`cmake --build $(build_DIR)`)
 
 
