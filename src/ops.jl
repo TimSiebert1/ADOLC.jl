a0ab129d924807a32d963875ce5258d8132d8f03
Tim Siebert <tim.siebert96@outlook.com>
2023-11-21 18:32:41 +0100

    adding vector init for independents and dependents

--- src/ops.jl
+++ src/ops.jl
@@ -3,3 +3,22 @@ Base.setindex!(X::CxxPtr{CxxPtr{Float64}}, val::Float64, row::Int64, col::Int64)
 Base.getindex(X::CxxPtr{Float64}, row::Int64) = getindex_vec(X, row)
 Base.setindex!(X::CxxPtr{Float64}, val::Float64, row::Int64) = setindex_vec(X, val, row)
   
+
+
+# convient inits for independant and dependants
+function init_independent_vec(a::Vector{Main.ADOLC_wrap.adoubleAllocated}, x::Vector{Float64})
+    for i in 1:length(x)
+        a[i] << x[i]
+    end
+end
+Base.:<<(a::Vector{Main.ADOLC_wrap.adoubleAllocated}, x::Vector{Float64}) = init_independent_vec(a, x)
+
+function init_dependent_vec(a::Vector{Main.ADOLC_wrap.adoubleAllocated}, x::Vector{Float64})
+    for i in 1:length(x)
+        a[i] >> x[i]
+    end
+end
+Base.:>>(a::Vector{Main.ADOLC_wrap.adoubleAllocated}, x::Vector{Float64}) = init_dependent_vec(a, x)
+
+
+
