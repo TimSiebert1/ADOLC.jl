a4b529447518eb22547aad51567d942cac94e3f7
Tim Siebert <tim.siebert96@outlook.com>
2024-02-10 12:25:37 +0100

    change name

---
+++ test/test_tladouble_arithmetics.jl
@@ -0,0 +1,55 @@
+using ADOLC.TladoubleModule
+using Test
+using IrrationalConstants
+
+
+#### test with passie adouble type
+
+a = Tladouble(3.0)
+@test (a + 3.0).val == 6.0
+@test typeof(a + 3.0) == Tladouble
+@test typeof((a + 3.0).val) == Float64
+@test typeof((a + invsqrt2).val) == Float64
+@test (a + invsqrt2 - invsqrt2).val == 3.0
+
+#### test with with active adouble type
+
+a = Tladouble(3.0, true)
+@test (a + 3.0).val == 6.0
+@test typeof(a + 3.0) == Tladouble
+@test typeof((a + 3.0).val) == TladoubleModule.TladoubleCxxAllocated
+@test typeof((a + invsqrt2).val) == TladoubleModule.TladoubleCxxAllocated
+@test (a + invsqrt2 - invsqrt2).val == 3.0
+
+
+#### test with with active and passive adouble type
+
+b = Tladouble(-3.6)
+@test isapprox(getValue((a + b).val), -0.6)
+@test typeof((a + b).val) == TladoubleModule.TladoubleCxxAllocated
+@test typeof((a + b)) == Tladouble
+
+
+c = [Tladouble(float(i)) for i in 1:10]
+setindex!(A::Vector{Tladouble}, x::Tladouble, i1::Int64)
+
+Tladouble(float(1))
+
+
+sinh(a.val)
+TladoubleModule.erf(a.val)
+
+
+
+using ADOLC
+using ADOLC.TladoubleModule
+
+_erfc(x::Tladouble) = erf(x)
+
+
+using StatsFuns
+
+a = Tladouble(4.0, true)
+
+
+normcdf(a)
