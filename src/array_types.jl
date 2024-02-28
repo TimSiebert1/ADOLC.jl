fe388011750d9d925ff243aded0c5368c1cd891e
Tim Siebert <tim.siebert96@outlook.com>
2024-02-28 15:27:55 +0100

    remove not existing functions

--- src/array_types.jl
+++ src/array_types.jl
@@ -121,6 +121,6 @@ Base.size(X::CxxVector{T}) where T <: Real = X.n
 Base.getindex(X::CxxVector{T}, row::Int64) where T <: Real = getindex(X.data, row)
 Base.setindex!(X::CxxVector{T}, val::T, row::Int64) where T <: Real = setindex!(X.data, val, row)
 
-export CxxMatrix, CxxVector, myalloc3, myalloc2, myalloc1, alloc_vec_double, alloc_vec_short, alloc_vec, alloc_mat_short
-export myfree3, myfree2, myfree1, free_vec_double, free_vec_short, free_mat_short
+export CxxMatrix, CxxVector, myalloc3, myalloc2, alloc_vec_double, alloc_vec_short, alloc_vec, alloc_mat_short
+export myfree3, myfree2, free_vec_double, free_vec_short, free_mat_short
 end # module arry_types
