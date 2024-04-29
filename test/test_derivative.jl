caae42ed076be8fd731a3b56f759293925c535e6
Tim Siebert <tim.siebert96@outlook.com>
2024-04-29 14:05:41 +0200

    update

--- test/test_derivative.jl
+++ test/test_derivative.jl
@@ -367,27 +367,27 @@ end
     
     x = [-0.5, -0.5, -0.5]
 
-    abs_normal_problem = ADOLC.AbsNormalForm()
-    derivative!(abs_normal_problem, f, 1, 3, x, :abs_normal)
+    abs_normal_form = ADOLC.init_abs_normal_form(f, 1, 3, x)
+    derivative!(abs_normal_form, f, 1, 3, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
 
-    @test abs_normal_problem.Y[1, 1] == -1.5
-    @test abs_normal_problem.Y[1, 2] == -3.0
-    @test abs_normal_problem.Y[1, 3] == -1.5
+    @test abs_normal_form.Y[1, 1] == -1.5
+    @test abs_normal_form.Y[1, 2] == -3.0
+    @test abs_normal_form.Y[1, 3] == -1.5
 
-    @test abs_normal_problem.J[1, 1] == 0.5
-    @test abs_normal_problem.J[1, 2] == 0.5
+    @test abs_normal_form.J[1, 1] == 0.5
+    @test abs_normal_form.J[1, 2] == 0.5
 
-    @test abs_normal_problem.Z[1, 1] == -1.0
-    @test abs_normal_problem.Z[1, 2] == -1.0
-    @test abs_normal_problem.Z[1, 3] == 0.0
-    @test abs_normal_problem.Z[2, 1] == 0.0
-    @test abs_normal_problem.Z[2, 2] == -1.0
-    @test abs_normal_problem.Z[2, 3] == -1.0
+    @test abs_normal_form.Z[1, 1] == -1.0
+    @test abs_normal_form.Z[1, 2] == -1.0
+    @test abs_normal_form.Z[1, 3] == 0.0
+    @test abs_normal_form.Z[2, 1] == 0.0
+    @test abs_normal_form.Z[2, 2] == -1.0
+    @test abs_normal_form.Z[2, 3] == -1.0
 
-    @test abs_normal_problem.L[1, 1] == 0.0
-    @test abs_normal_problem.L[1, 2] == 0.0
-    @test abs_normal_problem.L[2, 1] == 0.0
-    @test abs_normal_problem.L[2, 2] == 0.0
+    @test abs_normal_form.L[1, 1] == 0.0
+    @test abs_normal_form.L[1, 2] == 0.0
+    @test abs_normal_form.L[2, 1] == 0.0
+    @test abs_normal_form.L[2, 2] == 0.0
 end
 
 
@@ -399,37 +399,37 @@ end
     
     x = [-1.5, -1.5, -1.5]
 
-    abs_normal_problem = ADOLC.AbsNormalForm()
-    derivative!(abs_normal_problem, f, 1, 3, x, :abs_normal)
+    abs_normal_form = ADOLC.init_abs_normal_form(f, 1, 3, x)
+    derivative!(abs_normal_form, f, 1, 3, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
     y = f(x)
 
-    @test abs_normal_problem.y[1] == y
+    @test abs_normal_form.y[1] == y
 
     x = [-0.5, -0.5, -0.5]
-    # reuse abs_normal_problem with same id and without retaping
-    derivative!(abs_normal_problem, f, 1, 3, x, :abs_normal, tape_id=abs_normal_problem.tape_id, reuse_tape=true)
+    # reuse abs_normal_form with same id and without retaping
+    derivative!(abs_normal_form, f, 1, 3, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
     y = f(x)
 
-    @test abs_normal_problem.y[1] == y
+    @test abs_normal_form.y[1] == y
 
-    @test abs_normal_problem.Y[1, 1] == -1.5
-    @test abs_normal_problem.Y[1, 2] == -3.0
-    @test abs_normal_problem.Y[1, 3] == -1.5
+    @test abs_normal_form.Y[1, 1] == -1.5
+    @test abs_normal_form.Y[1, 2] == -3.0
+    @test abs_normal_form.Y[1, 3] == -1.5
 
-    @test abs_normal_problem.J[1, 1] == 0.5
-    @test abs_normal_problem.J[1, 2] == 0.5
+    @test abs_normal_form.J[1, 1] == 0.5
+    @test abs_normal_form.J[1, 2] == 0.5
 
-    @test abs_normal_problem.Z[1, 1] == -1.0
-    @test abs_normal_problem.Z[1, 2] == -1.0
-    @test abs_normal_problem.Z[1, 3] == 0.0
-    @test abs_normal_problem.Z[2, 1] == 0.0
-    @test abs_normal_problem.Z[2, 2] == -1.0
-    @test abs_normal_problem.Z[2, 3] == -1.0
+    @test abs_normal_form.Z[1, 1] == -1.0
+    @test abs_normal_form.Z[1, 2] == -1.0
+    @test abs_normal_form.Z[1, 3] == 0.0
+    @test abs_normal_form.Z[2, 1] == 0.0
+    @test abs_normal_form.Z[2, 2] == -1.0
+    @test abs_normal_form.Z[2, 3] == -1.0
 
-    @test abs_normal_problem.L[1, 1] == 0.0
-    @test abs_normal_problem.L[1, 2] == 0.0
-    @test abs_normal_problem.L[2, 1] == 0.0
-    @test abs_normal_problem.L[2, 2] == 0.0
+    @test abs_normal_form.L[1, 1] == 0.0
+    @test abs_normal_form.L[1, 2] == 0.0
+    @test abs_normal_form.L[2, 1] == 0.0
+    @test abs_normal_form.L[2, 2] == 0.0
 end
 
 
@@ -441,7 +441,7 @@ end
 
 @testset "hess_1D" begin
     function f(x) 
-        return x[1]^3 + x[2]^2
+        return x[1]^3 + x[2]^2 * x[3]
     end
     res = myalloc2(3, 3)
     derivative!(res, f, 1, 3, [-1.0, 1.0, 2.0], :hess)
@@ -451,8 +451,8 @@ end
     @test res[3, 1] == 0.0
 
     @test res[1, 2] == 0.0
-    @test res[2, 2] == 2.0
-    @test res[3, 2] == 0.0
+    @test res[2, 2] == 4.0
+    @test res[3, 2] == 2.0 
 
     @test res[1, 3] == 0.0
     @test res[2, 3] == 0.0
@@ -461,15 +461,13 @@ end
 
 end
 
-
 """
-
 @testset "hess_2D" begin
     function f(x) 
         return [x[1]^3 + x[2]^2, 3*x[3]^3]
     end
     res = myalloc3(2, 3, 3)
-    derivative(f, 2, 3, [-1.0, 1.0, 2.0], :hess, res=res)
+    derivative!(res, f, 2, 3, [-1.0, 1.0, 2.0], :hess)
 
     @test res[1, 1, 1] == -6.0
     @test res[1, 2, 1] == 0.0
@@ -496,36 +494,156 @@ end
     @test res[2, 1, 3] == 0.0
     @test res[2, 2, 3] == 0.0
     @test res[2, 3, 3] == 36.0
+end
+
+"""
+
+
+@testset "hess_vec" begin
+    function f(x)
+        return [x[1]^2*x[2], x[1]*x[3]^3]
+    end
+
+    x = [1.0, 2.0, 2.0]
+    dir = [-1.0, 1.0, 3.0]
+
+    res = myalloc2(2, 3)
+    derivative!(res, f, 2, 3, x, :hess_vec, dir=dir)
+
+    @test res[1, 1] == -2.0
+    @test res[1, 2] == -2.0
+    @test res[1, 3] == 0.0
+
+    @test res[2, 1] == 36.0
+    @test res[2, 2] == 0.0
+    @test res[2, 3] == 24.0
+end
+
+
+@testset "vec_hess_vec" begin
+    function f(x)
+        return [x[1]^2*x[2], x[1]*x[3]^3]
+    end
+
+    x = [1.0, 2.0, 2.0]
+    dir = [-1.0, 1.0, 3.0]
+    weights = [2.0, 1.0]
+    res = alloc_vec_double(3)
+    derivative!(res, f, 2, 3, x, :vec_hess_vec, dir=dir, weights=weights)
+
+    @test res[1] == 32.0
+    @test res[2] == -4.0
+    @test res[3] == 24.0
+
+end
+
+
+
+@testset "mat_hess_vec" begin
+    function f(x)
+        return [x[1]^2*x[2], x[1]*x[3]^3]
+    end
+
+    # 4 2 0 
+    # 2 0 0 
+    # 0 0 0
+    
+    # 0  0   12
+    # 0  0   0 
+    # 12 0   12
+
+    # -2.0 -2.0 0.0
+    # 36.0 0.0 24.0 
+
+
+    # -2.0 -2.0 0.0
+    # 36.0 0.0 24.0
+    # -36.0 0.0 -24.0 
+    x = [1.0, 2.0, 2.0]
+    dir = [-1.0, 1.0, 3.0]
+    weights = Matrix{Float64}(undef, 3, 2)
+    for i in 1:3
+        for j in 1:2
+            weights[i, j] = 0.0
+            if i == j
+                weights[i, i] = 1.0
+            end
+        end
+    end
+    weights[3, 2] = -1.0
+    res = myalloc2(3, 3)
+    derivative!(res, f, 2, 3, x, :mat_hess_vec, dir=dir, weights=weights)
+
+    @test res[1, 1] == -2.0
+    @test res[1, 2] == -2.0
+    @test res[1, 3] == 0.0
+
+    @test res[2, 1] == 36.0
+    @test res[2, 2] == 0.0
+    @test res[2, 3] == 24.0
+
+    @test res[3, 1] == -36.0
+    @test res[3, 2] == 0.0
+    @test res[3, 3] == -24.0
+end
+
+
+
+
+
+
+"""
+
+@testset "1D_hess_mat" begin()
+    function f(x)
+        return x[1]*x[3]^3
+    end
+    # 0  0   12
+    # 0  0   0 
+    # 12 0   12
+
+    x = [1.0, 2.0, 2.0]
+    dir = Matrix{Float64}(undef, 3, 2)
+    for i in 1:3
+        for j in 1:2
+            weights[i, j] = 0.0
+            if i == j
+                weights[i, i] = 1.0
+            end
+        end
+    end
+    weights[3, 2] = -1.0
+    res = myalloc2(3, 2)
+    derivative!(res, f, 1, 3, x, :hess_mat, dir=dir)
+
+    @test res[1, 1] == 0.0
+    @test res[1, 2] == -12.0
+
+    @test res[2, 1] == 0.0
+    @test res[2, 2] == 0.0
+
+    @test res[3, 1] == 12.0
+    @test res[3, 2] == 0.0
 
 end
 
 
 
 
-function test_second_order_hess_vec()
-    derivative(f, 2, 3, [1.0, 1.0, 2.0], :hess_vec, [-1.0, 1.0], res)
-end
-function test_second_order_hess_mat()
-    derivative(f, 2, 3, [1.0, 1.0, 2.0], :hess_mat, [-1.0, 1.0], res)
-end
 function test_second_order_vec_hess()
     derivative(f, 2, 3, [1.0, 1.0, 2.0], :vec_hess, [-1.0, 1.0], res)
 end
 function test_second_order_mat_hess()
     derivative(f, 2, 3, [1.0, 1.0, 2.0], :mat_hess, [-1.0, 1.0], res)
 end
-function test_second_order_vec_hess_vec()
-    derivative(f, 2, 3, [1.0, 1.0, 2.0], :vec_hess_vec, [-1.0, 1.0], res)
-end
+
 function test_second_order_vec_hess_mat()
     derivative(f, 2, 3, [1.0, 1.0, 2.0], :vec_hess_mat, [-1.0, 1.0], res)
 end
 function test_second_order_mat_hess_mat()
     derivative(f, 2, 3, [1.0, 1.0, 2.0], :mat_hess_mat, [-1.0, 1.0], res)
 end
-function test_second_order_mat_hess_vec()
-    derivative(f, 2, 3, [1.0, 1.0, 2.0], :mat_hess_vec, [-1.0, 1.0], res)
-end
+
 
 
 
