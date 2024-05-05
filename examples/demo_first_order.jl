94e233133ebba33bffa7bfb82e38651fec8c7d72
Tim Siebert <tim.siebert96@outlook.com>
2024-05-05 15:44:29 +0200

    update example

--- examples/demo_first_order.jl
+++ examples/demo_first_order.jl
@@ -5,10 +5,11 @@ function demo_first_order()
     function f(x)
         return [x[1]^2 + x[2], x[3]^3]
     end
-    res = ADOLC.myalloc2(2, 3)
+    x = [1.0, 1.0, 2.0]
+    
     dir = [[1.0, 0.0, 0.0] [-1.0, 1.0, 0.0] [0.0, 0.0, 1.0]]
-
-    derivative!(res, f, 2, 3, [1.0, 1.0, 2.0], :jac_mat, dir = dir)
+    res = ADOLC.myalloc2(2, 3)
+    derivative!(res, f, 2, 3, x, :jac_mat, dir = dir)
     for i in 1:2
         println("dim $i")
         for j in 1:3
