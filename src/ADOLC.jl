4f3a124a26ed981c8da83c9fd6a3aa806f3cd37e
Tim Siebert <tim.siebert96@outlook.com>
2024-06-04 15:11:38 +0200

    remove unused

--- src/ADOLC.jl
+++ src/ADOLC.jl
@@ -23,13 +23,11 @@ include("abs_normal.jl")
 export abs_normal!, AbsNormalForm
 
 include("utils.jl")
-include("univariate_tpp.jl")
 include("derivative.jl")
 export derivative!
 
-export gradient, _gradient_tape_based, _gradient_tape_less
-export _higher_order, tensor_address2, build_tensor, create_cxx_identity
-export taylor_coeff, check_input_taylor_coeff
+
+export tensor_address, create_cxx_identity
 export erf, eps
 
 end # module ADOLC
