e70c28cd94bbbc03402228e306c224a3b7a5d15b
Tim Siebert <tim.siebert96@outlook.com>
2024-06-07 18:31:47 +0200

    rm unused tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,5 @@ using ADOLC.array_types
 using Test
 
 include("test_adouble.jl")
-#include("test_gradient.jl")
-#include("test_hess_vec.jl")
-#include("test_reverse.jl")
-#include("test_taylor_coeff.jl")
-#include("test_tensor_eval.jl")
 include("test_derivative.jl")
-include("test_reuse_tape.jl")
+
