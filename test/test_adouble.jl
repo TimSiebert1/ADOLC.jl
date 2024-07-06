c500ae2da22f3b66489d5981d3030b4d2800c7c2
Tim Siebert <tim.siebert96@outlook.com>
2024-07-06 12:50:54 +0200

    add tests

--- test/test_adouble.jl
+++ test/test_adouble.jl
@@ -42,3 +42,28 @@
         @test typeof(A) == Vector{Adouble{t}}
     end
 end
+
+
+@testset "type handling" begin()
+
+    for t in [Adouble{TlAlloc}, Adouble{TbAlloc}]
+        a = t(3.0, adouble=true)
+        @test typeof(promote(1, a).val) == Cdouble
+        @test typeof(promote(1, a)) == t
+
+        b = t(3.0, adouble=false)
+        @test typeof(promote(1, b).val) == Cdouble
+        @test typeof(promote(1, b)) == t
+
+        @test typeof(promote(a, b)) == t
+        @test typeof(promote(a, b).val) == typeof(a.val)
+
+        @test Base.promote_rule(t, Real) == t
+        @test Base.promote_op(x -> x, t, t) == t
+        @test Base.promote_op(x -> x, Int64, t) == t
+        @test Base.promote_op(x -> x, t, Int64) == t
+        @test typeof(convert(t, 1).val) == Cdouble
+        @test typeof(convert(t, 1)) == t
+        @test convert(t, b) === b
+    end
+end
