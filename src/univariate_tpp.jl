43100e83200bcf3817b71d4eeb116a6916de755d
Tim Siebert <tim.siebert96@outlook.com>
2024-07-11 13:42:20 +0200

    add interfaces

--- src/univariate_tpp.jl
+++ src/univariate_tpp.jl
@@ -1,4 +1,43 @@
 
+"""
+    univariate_tpp(
+        f,
+        degree::Integer,
+        x::Union{Cdouble,Vector{Cdouble}};
+        keep::Bool=false,
+        tape_id::Integer=0,
+        reuse_tape::Bool=false,
+    )
+"""
+function univariate_tpp(
+    f,
+    degree::Integer,
+    x::Union{Cdouble,Vector{Cdouble}};
+    keep::Bool=false,
+    tape_id::Integer=0,
+    reuse_tape::Bool=false,
+)
+    if !reuse_tape
+        m, n = create_tape(f, x, tape_id)
+    else
+        m = TbadoubleModule.num_dependents(tape_id)
+        n = TbadoubleModule.num_independents(tape_id)
+    end
+    init_tp = CxxMatrix(zeros(Cdouble, n, degree + 1))
+    for j in 1:n
+        for i in 1:n
+            if j == 1
+                init_tp[i, j] = x[i]
+            elseif j == 2
+                init_tp[i, j] = 1.0
+            end
+        end
+    end
+    res = CxxMatrix(m, degree + 1)
+    univariate_tpp!(res, f, degree, x, init_tp; keep=keep, tape_id=tape_id, reuse_tape=true)
+    return res
+end
+
 
 """
     univariate_tpp(
